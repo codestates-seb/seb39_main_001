c1f66146f9ef971d43917ef846ee2f5ea55e34fe
thom-window <hsbang.thom@gmail.com>
2022-09-14 10:34:38 +0900

    initial commit

---
+++ server/juse/src/main/java/com/example/juse/JuseApplication.java
@@ -0,0 +1,13 @@
+package com.example.juse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuseApplication.class, args);
+	}
+
+}
---
+++ server/juse/src/test/java/com/example/juse/JuseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
