52910bf21cd56f0d56f7b9b4493d641befedcea6
aprochoi <101087622+aprochoi@users.noreply.github.com>
2022-09-15 17:44:46 +0900

    OAuth No Token (#8)

---
+++ server/juse/src/main/java/com/example/juse/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.juse.user.repository;
+
+import com.example.juse.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+
+}
