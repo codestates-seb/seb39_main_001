a46a3d6aac2242335b5719259edecd143b5f618d
cleats01 <cleats01@gmail.com>
2022-09-16 16:57:25 +0900

    feat: 회원가입 페이지, 기술 스택 셀렉터

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Join from './pages/Join';
+
 function App() {
-  return <div className='App'>hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/join" element={<Join />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
