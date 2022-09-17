d387b93d58a9e3077ca8300fcc4a8a93a0d735b3
ekim49 <hailieejkim@gmail.com>
2022-09-17 11:25:33 +0900

    design: public navbar 및 login modal CSS 초안

--- client/src/components/LoginModal.jsx
+++ client/src/components/LoginModal.jsx
@@ -1,10 +1,43 @@
 import axios from 'axios';
 import styled from 'styled-components';
+import google from '../assets/images/google.svg';
 
+const LoginModal = ({ showModal, setShowModal }) => {
+  const LoginGoogle = axios
+    .post('/oauth2/authorization/google')
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+
+  const closeModal = () => {
+    setShowModal(!showModal);
+  };
+
+  return (
+    <>
+      {showModal ? (
+        <Background onClick={closeModal}>
+          <LoginModalContainer showModal={showModal}>
+            <CloseBtn onClick={closeModal}>
+              <i class="fi fi-sr-cross-circle"></i>
+            </CloseBtn>
+            <p>환영합니다</p>
+            <GoogleLoginBtn onClick={LoginGoogle}>
+              <GoogleLogo />
+              <p>Google 계정으로 로그인</p>
+            </GoogleLoginBtn>
+          </LoginModalContainer>
+        </Background>
+      ) : null}
+    </>
+  );
+};
+// Login Modal이 열렸을 때 뒷 배경
 const Background = styled.div`
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.4);
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.1);
   position: fixed;
   display: flex;
   justify-content: center;
@@ -17,31 +50,53 @@ const LoginModalContainer = styled.div`
   justify-content: space-evenly;
   align-items: center;
   width: 500px;
-  height: 600px;
-  border: 1px solid black;
+  height: 300px;
+  border-radius: 4px;
+  box-shadow: 0 0 10px 3px rgba(0, 0, 0, 0.1);
   position: relative;
   background: white;
+  > p {
+    font-size: 20px;
+    font-weight: 600;
+    color: ${({ theme }) => theme.colors.grey4};
+  }
 `;
 
-const LoginModal = ({ showModal, setShowModal }) => {
-  const LoginGoogle = axios
-    .post(`/oauth2/authorization/google`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+const CloseBtn = styled.div`
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  color: ${({ theme }) => theme.colors.grey2};
+  cursor: pointer;
+  :hover {
+    color: ${({ theme }) => theme.colors.grey3};
+  }
+`;
 
-  return (
-    <>
-      {showModal ? (
-        <Background>
-          <LoginModalContainer showModal={showModal}>
-            <div>로그인 하세용</div>
-            <button onClick={LoginGoogle}>Google 로그인</button>
-            <button onClick={() => setShowModal(!showModal)}>닫기</button>
-          </LoginModalContainer>
-        </Background>
-      ) : null}
-    </>
-  );
-};
+const GoogleLogo = styled.img.attrs({
+  src: `${google}`,
+})`
+  width: 18px;
+  height: 18px;
+  margin-right: 10px;
+`;
+
+const GoogleLoginBtn = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px 20px;
+  width: 300px;
+  border: 1px solid ${({ theme }) => theme.colors.grey2};
+  border-radius: 4px;
+  cursor: pointer;
+  :hover {
+    border: 1px solid ${({ theme }) => theme.colors.grey3};
+  }
+  > p {
+    color: ${({ theme }) => theme.colors.black1};
+    font-size: 14px;
+  }
+`;
 
 export default LoginModal;
--- client/src/components/NavbarPublic.jsx
+++ client/src/components/NavbarPublic.jsx
@@ -1,18 +1,7 @@
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import styled from 'styled-components';
 import LoginModal from './LoginModal';
-
-const NavbarContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border: 1px solid blue;
-`;
-
-const LoginButton = styled.button`
-  width: 100px;
-  cursor: pointer;
-`;
 
 const NavbarPublic = () => {
   const [showModal, setShowModal] = useState(false);
@@ -23,11 +12,44 @@ const NavbarPublic = () => {
 
   return (
     <NavbarContainer>
-      <div>JUSE</div>
+      <LogoLink to="/">
+        JU:SE
+        <p>Junior to Senior</p>
+      </LogoLink>
       <LoginButton onClick={openModal}>로그인</LoginButton>
       <LoginModal showModal={showModal} setShowModal={setShowModal} />
     </NavbarContainer>
   );
 };
+
+const NavbarContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.1);
+`;
+
+const LoginButton = styled.button`
+  width: 80px;
+  background-color: #ffffff;
+  border: 1px solid ${({ theme }) => theme.colors.purple1};
+  color: ${({ theme }) => theme.colors.purple1};
+  border-radius: 4px;
+  cursor: pointer;
+  :hover {
+    border: 1px solid ${({ theme }) => theme.colors.purple2};
+    color: ${({ theme }) => theme.colors.purple2};
+  }
+`;
+
+const LogoLink = styled(Link)`
+  font-size: 30px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.colors.purple1};
+  > p {
+    font-size: 10px;
+  }
+`;
 
 export default NavbarPublic;
