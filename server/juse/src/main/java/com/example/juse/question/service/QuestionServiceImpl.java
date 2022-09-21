ea13333da13093692d29594f1edfe278a1c6096e
thom-window <hsbang.thom@gmail.com>
2022-09-21 13:24:23 +0900

    feat : post question

--- server/juse/src/main/java/com/example/juse/question/service/QuestionServiceImpl.java
+++ server/juse/src/main/java/com/example/juse/question/service/QuestionServiceImpl.java
@@ -1,7 +1,11 @@
 package com.example.juse.question.service;
 
+import com.example.juse.board.entity.Board;
+import com.example.juse.board.service.BoardService;
 import com.example.juse.question.entity.Question;
 import com.example.juse.question.repository.QuestionRepository;
+import com.example.juse.user.entity.User;
+import com.example.juse.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
@@ -12,10 +16,21 @@ import org.springframework.stereotype.Service;
 public class QuestionServiceImpl implements QuestionService{
 
     private final QuestionRepository questionRepository;
+    private final BoardService boardService;
+    private final UserService userService;
 
     @Override
     public Question create(Question post) {
-        return null;
+        long boardId = post.getBoard().getId();
+        long userId = post.getUser().getId();
+
+        Board board = boardService.verifyBoardById(boardId);
+        User user = board.getUser();
+
+        post.addBoard(board);
+        post.addUser(user);
+
+        return questionRepository.save(post);
     }
 
     @Override
