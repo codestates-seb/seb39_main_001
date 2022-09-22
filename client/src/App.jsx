26908588729a5a0e96a02f9056576ceac1b6f43b
ekim49 <hailieejkim@gmail.com>
2022-09-22 15:05:13 +0900

    design: Navbar maxwidth 조정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,29 +9,33 @@ import Board from './pages/Board';
 import OAuth from './pages/OAuth';
 import UserInfo from './pages/UserInfo';
 import EditUser from './pages/EditUser';
+// import EditNewMeeting from './pages/EditNewMeeting';
 import { useCookies } from 'react-cookie';
 
 function App() {
-  // TODO: 서버로 부터 token 받으면 클라이언트에 cookie에 담기
-  const [cookies, setCookie, removeCookie] = useCookies(['user']);
+	// TODO: 서버로 부터 token 받으면 클라이언트에 cookie에 담기
+	const [cookies, setCookie, removeCookie] = useCookies(['user']);
 
-  return (
-    <>
-      {cookies.user ? <NavbarPrivate removeCookie={removeCookie} /> : <NavbarPublic />}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        {/* OAuth 로그인 후 redirection */}
-        {/* <Route path='/oauth2/redirect' element={<Home />} /> */}
-        <Route path='/join' element={<Join />} />
-        <Route path='/boards/:boardId' element={<Board />} />
-        <Route path='/oauth2/redirect' element={<OAuth />} />
-        <Route path='/users/' element={<UserInfo />} />
-        <Route path='/users/:userId' element={<UserInfo />} />
-        <Route path='/users/edit' element={<EditUser />} />
-        <Route path='/boards' element={<NewMeeting />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			{cookies.user ? (
+				<NavbarPrivate removeCookie={removeCookie} />
+			) : (
+				<NavbarPublic />
+			)}
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/join' element={<Join />} />
+				<Route path='/boards/:boardId' element={<Board />} />
+				<Route path='/oauth2/redirect' element={<OAuth />} />
+				<Route path='/users/' element={<UserInfo />} />
+				<Route path='/users/:userId' element={<UserInfo />} />
+				<Route path='/users/edit' element={<EditUser />} />
+				<Route path='/boards' element={<NewMeeting />} />
+				{/* <Route path='/boards/edit' element={<EditNewMeeting />} /> */}
+			</Routes>
+		</>
+	);
 }
 
 export default App;
