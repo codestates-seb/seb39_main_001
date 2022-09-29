f372d1b0c3e30aeabcbb067da1c347850c0f7e5c
cleats01 <cleats01@gmail.com>
2022-09-29 17:29:20 +0900

    fix: 회원가입 성공 시 이동

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -44,11 +44,12 @@ const Join = () => {
     } else if (!user.introduction) {
       alert('한 줄 소개를 입력하세요.');
     } else {
-      apis.postJoin(token, user);
-      // .then(alert('회원가입에 성공하였습니다.'))
-      // .then(() => {
-      //   navigate('/');
-      // });
+      apis
+        .postJoin(token, user)
+        .then(alert('회원가입에 성공하였습니다. 다시 로그인해 주세요.'))
+        .then(() => {
+          navigate('/');
+        });
     }
   };
 
