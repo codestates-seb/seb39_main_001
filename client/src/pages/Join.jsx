019a2aba6636fc5410c12e0a4994dcfc0b7bb3e8
ekim49 <hailieejkim@gmail.com>
2022-10-04 15:45:48 +0900

    style: join 페이지 css 수정

--- client/src/pages/Join.jsx
+++ client/src/pages/Join.jsx
@@ -5,251 +5,310 @@ import { apis } from '../apis/axios';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Join = () => {
-  const [imageSrc, setImageSrc] = useState('/icons/img/user-default.png');
-  const [imageFile, setImageFile] = useState(null);
-  const [stack, setStack] = useState([]);
-  const [nickname, setNickname] = useState('aasdf');
-  const [portfolio, setPortfolio] = useState('');
-  const [introduction, setIntroduction] = useState('');
-  const [isValid, setIsValid] = useState(true);
-  const [isValid2, setIsValid2] = useState(false);
-  const [validMsg, setValidMsg] = useState('닉네임을 입력해주세요.');
-  const navigate = useNavigate();
-  const location = useLocation();
+	const [imageSrc, setImageSrc] = useState('/icons/img/user-default.png');
+	const [imageFile, setImageFile] = useState(null);
+	const [stack, setStack] = useState([]);
+	const [nickname, setNickname] = useState('aasdf');
+	const [portfolio, setPortfolio] = useState('');
+	const [introduction, setIntroduction] = useState('');
+	const [isValid, setIsValid] = useState(true);
+	const [isValid2, setIsValid2] = useState(false);
+	const [validMsg, setValidMsg] = useState('닉네임을 입력해주세요.');
+	const navigate = useNavigate();
+	const location = useLocation();
 
-  // OAuth 페이지에서 넘겨준 토큰
-  const token = location.state.token;
+	// OAuth 페이지에서 넘겨준 토큰
+	const token = location.state.token;
 
-  //유저 데이터
-  const user = {
-    nickname,
-    portfolio,
-    introduction,
-    skillStackTags: stack,
-  };
+	//유저 데이터
+	const user = {
+		nickname,
+		portfolio,
+		introduction,
+		skillStackTags: stack,
+	};
 
-  //인풋 핸들러들
-  const nicknameHandler = (e) => {
-    setNickname(e.target.value);
-  };
-  const portfolioHandler = (e) => {
-    setPortfolio(e.target.value);
-  };
-  const introductionHandler = (e) => {
-    setIntroduction(e.target.value);
-  };
+	//인풋 핸들러들
+	const nicknameHandler = (e) => {
+		setNickname(e.target.value);
+	};
+	const portfolioHandler = (e) => {
+		setPortfolio(e.target.value);
+	};
+	const introductionHandler = (e) => {
+		setIntroduction(e.target.value);
+	};
 
-  // 회원가입 submit
-  const postJoinHandler = () => {
-    if (!user.nickname) {
-      alert('닉네임을 입력하세요.');
-    } else if (!user.introduction) {
-      alert('한 줄 소개를 입력하세요.');
-    } else {
-      if (!isValid || !isValid2) {
-        alert('닉네임을 확인해주세요.');
-      } else {
-        apis
-          .postJoin(token, user, imageFile)
-          .then(alert('회원가입에 성공하였습니다. 다시 로그인해 주세요.'))
-          .then(() => {
-            navigate('/');
-          });
-      }
-    }
-  };
+	// 회원가입 submit
+	const postJoinHandler = () => {
+		if (!user.nickname) {
+			alert('닉네임을 입력하세요.');
+		} else if (!user.introduction) {
+			alert('한 줄 소개를 입력하세요.');
+		} else {
+			if (!isValid || !isValid2) {
+				alert('닉네임을 확인해주세요.');
+			} else {
+				apis
+					.postJoin(token, user, imageFile)
+					.then(alert('회원가입에 성공하였습니다. 다시 로그인해 주세요.'))
+					.then(() => {
+						navigate('/');
+					});
+			}
+		}
+	};
 
-  // 닉네임 유효성 검사
-  const validationMessage = (nickname) => {
-    const regex = /^[가-힣|a-z|A-Z|0-9|]+$/;
-    if (nickname.length === 0) {
-      setIsValid2(false);
-      setValidMsg('닉네임을 입력해주세요.');
-    } else if (nickname.length < 2) {
-      setIsValid2(false);
-      setValidMsg('닉네임은 2자 이상 입력해주세요.');
-    } else if (nickname.length > 10) {
-      setIsValid2(false);
-      setValidMsg('닉네임은 10자 이내로 입력해주세요.');
-    } else if (!regex.test(nickname)) {
-      setIsValid2(false);
-      setValidMsg('닉네임은 한글,영어,숫자만 가능합니다.');
-    } else {
-      setIsValid2(true);
-      setValidMsg('사용가능한 닉네임입니다.');
-    }
-    return;
-  };
+	// 닉네임 유효성 검사
+	const validationMessage = (nickname) => {
+		const regex = /^[가-힣|a-z|A-Z|0-9|]+$/;
+		if (nickname.length === 0) {
+			setIsValid2(false);
+			setValidMsg('닉네임을 입력해주세요.');
+		} else if (nickname.length < 2) {
+			setIsValid2(false);
+			setValidMsg('닉네임은 2자 이상 입력해주세요.');
+		} else if (nickname.length > 10) {
+			setIsValid2(false);
+			setValidMsg('닉네임은 10자 이내로 입력해주세요.');
+		} else if (!regex.test(nickname)) {
+			setIsValid2(false);
+			setValidMsg('닉네임은 한글,영어,숫자만 가능합니다.');
+		} else {
+			setIsValid2(true);
+			setValidMsg('사용가능한 닉네임입니다.');
+		}
+		return;
+	};
 
-  // 중복확인
-  useEffect(() => {
-    apis.getNickname(nickname).then((data) => {
-      console.log(data);
-      setIsValid(data);
-    });
-    validationMessage(nickname);
-  }, [nickname]);
+	// 중복확인
+	useEffect(() => {
+		apis.getNickname(nickname).then((data) => {
+			console.log(data);
+			setIsValid(data);
+		});
+		validationMessage(nickname);
+	}, [nickname]);
 
-  // 프로필 이미지 업로드
-  const fileInput = useRef(null);
-  const imageButtonHandler = () => {
-    fileInput.current.click();
-  };
-  const imageChangeHandler = (e) => {
-    if (e.target.files[0]) {
-      setImageFile(e.target.files[0]);
-    }
-    //바뀐 이미지 렌더
-    const reader = new FileReader();
-    reader.onload = () => {
-      if (reader.readyState === 2) {
-        setImageSrc(reader.result);
-      }
-    };
-    reader.readAsDataURL(e.target.files[0]);
-  };
-  const imageDeleteHandler = () => {
-    setImageFile(null);
-    setImageSrc('/icons/img/user-default.png');
-  };
+	// 프로필 이미지 업로드
+	const fileInput = useRef(null);
+	const imageButtonHandler = () => {
+		fileInput.current.click();
+	};
+	const imageChangeHandler = (e) => {
+		if (e.target.files[0]) {
+			setImageFile(e.target.files[0]);
+		}
+		//바뀐 이미지 렌더
+		const reader = new FileReader();
+		reader.onload = () => {
+			if (reader.readyState === 2) {
+				setImageSrc(reader.result);
+			}
+		};
+		reader.readAsDataURL(e.target.files[0]);
+	};
+	const imageDeleteHandler = () => {
+		setImageFile(null);
+		setImageSrc('/icons/img/user-default.png');
+	};
 
-  return (
-    <div>
-      <JoinContainer>
-        <JoinHeader>
-          <h2>회원가입</h2>
-        </JoinHeader>
-        <ProfileUpload>
-          <p>프로필 사진</p>
-          <div className='uploader'>
-            <ImageContainer>
-              <img
-                src={
-                  imageSrc === 'default.jpg'
-                    ? '/icons/img/user-default.png'
-                    : imageSrc
-                }
-                alt='프로필'
-              />
-            </ImageContainer>
-            <input
-              type='file'
-              style={{ display: 'none' }}
-              accept='image/*'
-              onChange={imageChangeHandler}
-              ref={fileInput}
-            />
-            <StyledButton onClick={imageButtonHandler}>업로드</StyledButton>
-            <StyledButton onClick={imageDeleteHandler}>
-              업로드 삭제
-            </StyledButton>
-          </div>
-        </ProfileUpload>
-        <JoinInput>
-          <p>닉네임</p>
-          <input type='text' onChange={nicknameHandler}></input>
-          <Validation className={isValid && isValid2 ? 'valid' : 'not-valid'}>
-            {isValid ? validMsg : '중복된 닉네임이 존재합니다.'}
-          </Validation>
-          <p>포트폴리오 링크 (깃헙, 노션, 블로그...)</p>
-          <input type='text' onChange={portfolioHandler}></input>
-          <p>기술 스택</p>
-          <TechStack selected={stack} setSelected={setStack} />
-          <p>한 줄 소개</p>
-          <textarea type='text' onChange={introductionHandler}></textarea>
-        </JoinInput>
-        <StyledButton onClick={postJoinHandler}>회원 가입</StyledButton>
-      </JoinContainer>
-    </div>
-  );
+	return (
+		<div>
+			<JoinContainer>
+				<JoinHeader>
+					<h2>회원가입</h2>
+				</JoinHeader>
+				<ProfileUpload>
+					<p>프로필 사진</p>
+					<div className='uploader'>
+						<ImageContainer>
+							<img
+								src={
+									imageSrc === 'default.jpg'
+										? '/icons/img/user-default.png'
+										: imageSrc
+								}
+								alt='프로필'
+							/>
+						</ImageContainer>
+						<input
+							type='file'
+							style={{ display: 'none' }}
+							accept='image/*'
+							onChange={imageChangeHandler}
+							ref={fileInput}
+						/>
+						<StyledButton className='upload-btn' onClick={imageButtonHandler}>
+							업로드
+						</StyledButton>
+						<StyledButton
+							className='upload-cancel-btn'
+							onClick={imageDeleteHandler}
+						>
+							업로드 삭제
+						</StyledButton>
+					</div>
+				</ProfileUpload>
+				<JoinInput>
+					<p>
+						닉네임<span>(필수)</span>
+					</p>
+					<input type='text' onChange={nicknameHandler}></input>
+					<Validation className={isValid && isValid2 ? 'valid' : 'not-valid'}>
+						{isValid ? validMsg : '중복된 닉네임이 존재합니다.'}
+					</Validation>
+					<p>포트폴리오 링크</p>
+					<input
+						type='text'
+						placeholder='GitHub, Notion, Blog...'
+						onChange={portfolioHandler}
+					></input>
+					<p>기술 스택</p>
+					<TechStack selected={stack} setSelected={setStack} />
+					<p>
+						한 줄 소개<span>(필수)</span>
+					</p>
+					<textarea
+						type='text'
+						placeholder='나를 소개해 주세요!'
+						onChange={introductionHandler}
+					></textarea>
+				</JoinInput>
+				<StyledButton className='join-btn' onClick={postJoinHandler}>
+					회원 가입
+				</StyledButton>
+			</JoinContainer>
+		</div>
+	);
 };
 
 const JoinContainer = styled.div`
-  max-width: 1300px;
-  padding: 0 30px;
-  padding-bottom: 50px;
-  margin: auto;
+	max-width: 1300px;
+	padding: 0 30px;
+	padding-bottom: 50px;
+	margin: auto;
 `;
 
 const JoinHeader = styled.header`
-  border-bottom: 1px solid ${({ theme }) => theme.colors.grey2};
-  padding: 50px 0;
-  margin-bottom: 20px;
-  > h2 {
-    font-weight: 700;
-    font-size: 26px;
-  }
+	border-bottom: 1px solid ${({ theme }) => theme.colors.grey2};
+	padding: 50px 0;
+	margin-bottom: 20px;
+	> h2 {
+		font-weight: 700;
+		font-size: 26px;
+	}
 `;
 
 const ProfileUpload = styled.div`
-  > .uploader {
-    display: flex;
-    align-items: end;
-    margin: 10px 0;
-    > button {
-      margin-left: 10px;
-    }
-  }
+	> .uploader {
+		display: flex;
+		align-items: end;
+		margin: 10px 0;
+		> button {
+			margin-left: 10px;
+		}
+	}
 `;
 
 const ImageContainer = styled.div`
-  position: relative;
-  width: 150px;
-  height: 150px;
-  border: 1px solid ${({ theme }) => theme.colors.grey3};
-  border-radius: 999px;
-  > img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    transform: translate(50, 50);
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 999px;
-    margin: auto;
-    padding: 5px;
-  }
+	position: relative;
+	width: 150px;
+	height: 150px;
+	border: 1px solid ${({ theme }) => theme.colors.grey3};
+	border-radius: 999px;
+	> img {
+		position: absolute;
+		top: 0;
+		left: 0;
+		transform: translate(50, 50);
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+		border-radius: 999px;
+		margin: auto;
+		padding: 5px;
+	}
 `;
 
 const JoinInput = styled.div`
-  input,
-  textarea {
-    border: 1px solid ${({ theme }) => theme.colors.grey3};
-    margin: 10px 0;
-    padding: 5px;
-  }
-  textarea {
-    min-width: 400px;
-    min-height: 100px;
-    resize: none;
-  }
+	input,
+	textarea {
+		border: 1px solid ${({ theme }) => theme.colors.grey3};
+		border-radius: 4px;
+		margin: 10px 0;
+		padding: 5px;
+		background: ${({ theme }) => theme.background};
+		color: ${({ theme }) => theme.text};
+		:focus {
+			outline-color: ${({ theme }) => theme.colors.purple1};
+		}
+		:focus::-webkit-input-placeholder {
+			color: transparent;
+		}
+	}
+	textarea {
+		min-width: 400px;
+		min-height: 100px;
+		resize: none;
+	}
+	> p {
+		/* margin: 10px 0px; */
+		> span {
+			color: tomato;
+			font-size: 10px;
+			margin-left: 5px;
+		}
+	}
 `;
 
 const Validation = styled.p`
-  font-size: 13px;
-  margin-bottom: 15px;
-  &.valid {
-    color: blueviolet;
-  }
-  &.not-valid {
-    color: tomato;
-  }
+	font-size: 13px;
+	margin-bottom: 15px;
+	&.valid {
+		color: blueviolet;
+	}
+	&.not-valid {
+		color: tomato;
+	}
 `;
 
 const StyledButton = styled.button`
-  padding: 5px 10px;
-  background: #ffffff;
-  font-size: 14px;
-  color: ${({ theme }) => theme.colors.black1};
-  border: 1px solid ${({ theme }) => theme.colors.grey3};
-  border-radius: 4px;
-  cursor: pointer;
-  :hover {
-    color: #ffffff;
-    border: 1px solid ${({ theme }) => theme.colors.purple1};
-    background: ${({ theme }) => theme.colors.purple1};
-  }
+	padding: 5px 10px;
+	background: ${({ theme }) => theme.background};
+	font-size: 14px;
+	color: ${({ theme }) => theme.colors.grey4};
+	border: 1px solid ${({ theme }) => theme.colors.grey3};
+	border-radius: 4px;
+	cursor: pointer;
+	:hover {
+		color: #ffffff;
+		border: 1px solid ${({ theme }) => theme.colors.purple1};
+		background: ${({ theme }) => theme.colors.purple1};
+	}
+	&.upload-btn {
+		:hover {
+			background: ${({ theme }) => theme.background};
+			border: 1px solid ${({ theme }) => theme.colors.purple1};
+			color: ${({ theme }) => theme.colors.purple1};
+		}
+	}
+	&.upload-cancel-btn {
+		:hover {
+			border: 1px solid ${({ theme }) => theme.colors.grey4};
+			background: ${({ theme }) => theme.background};
+			color: ${({ theme }) => theme.colors.grey4};
+		}
+	}
+	&.join-btn {
+		color: #ffffff;
+		border: 1px solid ${({ theme }) => theme.colors.purple1};
+		background: ${({ theme }) => theme.colors.purple1};
+		:hover {
+			background: #a571fe;
+			border: 1px solid #a571fe;
+		}
+	}
 `;
 
 export default Join;
